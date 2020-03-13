f2f151849a529bcb4623b2c5c62de88c9b22d983
sinainnet <sinainnet@gmail.com>
2020-03-13 14:31:45 +0330

    my files

---
+++ routing-info.h
@@ -0,0 +1,22 @@
+#ifndef ROUTING_INFO_H_
+#define ROUTING_INFO_H_
+
+//int rtnl_receive(int fd, struct msghdr *msg, int flags);
+//
+//static int rtnl_recvmsg(int fd, struct msghdr *msg, char **answer);
+//
+//void parse_rtattr(struct rtattr *tb[], int max, struct rtattr *rta, int len);
+//
+//static inline int rtm_get_table(struct rtmsg *r, struct rtattr **tb);
+//
+//int check_primary_route(struct nlmsghdr* nl_header_answer, char *if_name);
+//void print_route(struct nlmsghdr* nl_header_answer);
+//
+//int open_netlink();
+//
+//int do_route_dump_requst(int sock);
+//
+//int get_route_dump_check_primary(int sock, char ifa_name[MAX_PHYS_IFS][16], size_t if_numbers);
+//int get_route_dump_response(int sock);
+
+#endif
